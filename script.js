12dd4c6ab421e144a64b83eb2f7caf01da5dd284
julian <jdrc@protonmail.com>
2025-01-21 17:07:50 -0500

    misc fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.burger-container')
 const menu = document.querySelector('.header-links')
 const arrow = document.querySelector('.arrow')
+const heading = document.querySelector('.heading-text')
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('active')
     arrow.classList.toggle('visible')
+    heading.classList.toggle('visible')
 })
